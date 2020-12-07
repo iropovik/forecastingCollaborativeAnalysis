88c29386314cc484967b48cef4f5b43bef9731e5
Ivan Ropovik <ivan.ropovik@gmail.com>
2020-12-07 15:39:38 +0100

    checks for the autocorrelation of residuals incorporated

--- arimaModels.R
+++ arimaModels.R
@@ -10,30 +10,38 @@ politicalPolarization <- read_csv("data/Political Polarization/political_polariz
 
 # Life satisfaction
 mod_lifeSatisfaction <- auto.arima(lifeSatisfaction[,2], seasonal = FALSE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_lifeSatisfaction)
 forecast_lifeSatisfaction <- as.vector(forecast(mod_lifeSatisfaction, h = 12)$mean)
 
 # Affect
 # negativeAffect
 mod_negativeAffect <- auto.arima(affect[,2], seasonal = TRUE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_negativeAffect)
 forecast_negativeAffect <- as.vector(forecast(mod_negativeAffect, h = 12)$mean)
 # positiveAffect
 mod_positiveAffect <- auto.arima(abs(affect[,3]), seasonal = TRUE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_positiveAffect)
 forecast_positiveAffect <- -1*as.vector(forecast(mod_positiveAffect, h = 12)$mean)
 
 # Political Ideology
 # Support for democrats
 mod_ideologySuppDemocrats <- auto.arima(politicalIdeology[,2], seasonal = FALSE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_ideologySuppDemocrats)
 forecast_ideologySuppDemocrats <- as.vector(forecast(mod_ideologySuppDemocrats, h = 12)$mean)
 # Support for republicans
 mod_ideologySuppRepublicans <- auto.arima(politicalIdeology[,3], seasonal = FALSE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_ideologySuppRepublicans)
 forecast_ideologySuppRepublicans <- as.vector(forecast(mod_ideologySuppRepublicans, h = 12)$mean)
 
 # Political Polarization
 # Democrat approvals
 mod_politicalPolarizationDem <- auto.arima(politicalPolarization[,2], seasonal = TRUE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_politicalPolarizationDem)
 forecast_politicalPolarizationDem <- as.vector(forecast(mod_politicalPolarizationDem, h = 12)$mean)
 # Republican approvals
 mod_politicalPolarizationRep <- auto.arima(politicalPolarization[,3], seasonal = TRUE, stationary = TRUE, stepwise = FALSE, lambda = "auto", biasadj = TRUE)
+checkresiduals(mod_politicalPolarizationRep)
 forecast_politicalPolarizationRep <- as.vector(forecast(mod_politicalPolarizationRep, h = 12)$mean)
+
 forecast_politicalPolarization <- forecast_politicalPolarizationRep - forecast_politicalPolarizationDem
 
